8a070fbd737a8ed1fff7ab6cf9cf2f0b2ff6698b
Daniela Todorova <dtodorowa1705@gmail.com>
2022-09-15 10:34:48 +0100

    First Commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+
+<template class="about">
+  <div>
+    <h1>About</h1>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,94 @@
+
+<template class="home">
+  <div>
+  <h2>{{ appTitle }}</h2>
+  <h3>{{ counterData.title }}:</h3>
+    <div>
+      <button @click="decreaseCounter(2)" class="btn">--</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">++</button>
+    </div>
+
+    <p>This counter is {{oddOrEven}}</p>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { reactive, computed, watch,onBeforeMount,onMounted,onBeforeUnmount,onUnmounted, onActivated, onDeactivated } from 'vue';
+
+  const appTitle = 'My Amazing Counter App'
+
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  })
+
+  watch(() => counterData.count, (newCount, oldCount) => {
+    console.log('newCount', newCount);
+  })
+
+  const oddOrEven = computed(() => {
+    if (counterData.count % 2 === 0) return 'even'
+    return 'odd'
+  })
+
+  const increaseCounter = (amount, e) => {
+    counterData.count += amount;
+  }
+
+  const decreaseCounter = amount => {
+    counterData.count -= amount;
+  }
+
+  onBeforeMount(() => {
+    //before the component is mounted
+  })
+
+  onMounted(() => {
+
+  })
+
+  onBeforeUnmount(() => {
+
+  })
+
+  onUnmounted(() => {
+    
+  })
+
+  onActivated(() => {
+    //only fired if the component is kept alive  
+  })
+  
+  onDeactivated(() => {
+    //only fired if the component is kept alive  
+
+  })
+
+</script>
+
+
+
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+
+.edit{
+  margin-top: 60px;
+}
+</style>
